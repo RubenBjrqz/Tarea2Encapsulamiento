80fd1adfad665e5a8adefa22a09280d11ab30658
Rubén Bojórquez <42181319+RubenBjrqz@users.noreply.github.com>
2018-08-15 22:53:25 -0700

    Tarea2
    Tarea

---
+++ Tarea2Encapsulamiento/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2Encapsulamiento
+{
+    class Enemy
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Explode { get; set; }
+        public string Move { get; set; }
+        public string Shoot { get; set; }
+
+
+        private string _points;
+        public string Points
+        {
+            get
+            {
+                return _points;
+            }
+            set
+            {
+                if (value.Length < 100)
+                {
+                    _points = "Deberia dar+100";
+                }
+                else
+                {
+                    _points = "+100";
+                }
+            }
+        }
+    }
+}
---
+++ Tarea2Encapsulamiento/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2Encapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy Enemigo = new Enemy();
+
+            Enemigo.X = "Posicion X: ";
+            Enemigo.Y = "Posicion Y: ";
+            Enemigo.Move = "Se ha movio: ";
+            Enemigo.Shoot = "Disparo hacia: ";
+            Enemigo.Explode = "Exploto";
+
+            Console.ReadLine();
+        }
+    }
+}
+
